cd1f7031830eb72b06bc4db5bd343c46feb86145
Gabriel Balaban <gabebalaban@gmail.com>
2026-04-21 21:52:14 +0200

    experiment runner bugfix boundary conditions simparams only copied once

--- Experiments/3DSimulation_Benchmark/run_experiment.py
+++ Experiments/3DSimulation_Benchmark/run_experiment.py
@@ -36,22 +36,23 @@ def main(energy_function, scenario):
     run_nums = np.arange(args.run_start, args.run_end)
     lhs_subsample = lhs_sample.iloc[run_nums]
 
-    for i, lhs_params in lhs_subsample.iterrows():
-        simulation_params["energy_function"] = energy_function
-        simulation_params["material_parameters"] = dict(lhs_params) 
-        simulation_params["mesh"]["path"] =  f"3Dsimulation_data/{energy_function}/{scenario}/unloaded_geo.hdf5"
-        simulation_params["mesh"]["microstructure"] =  f"3Dsimulation_data/{energy_function}/{scenario}/unloaded_geo_microstructure.h5"
-        simulation_params["mechdata"] = f"3Dsimulation_data/pressure_traces/{scenario}_traces.csv"
-        simulation_params["output"]["files"]["path"] = f"results/{energy_function}/{scenario}/{i}"
-        simulation_params["output"]["files"]["logfile"] = f"results/{energy_function}/{scenario}/parameter_estimation.log"
+    #Pick scenario appropriate boundary conditions and compressibility
+    simulation_params["numerics"]["base_spring_k"] =  simulation_params["numerics"]["base_spring_k"][scenario]
+    simulation_params["numerics"]["compress"] =  simulation_params["numerics"]["compress"][scenario]
+    simulation_params["energy_function"] = energy_function
+    
+    simulation_params["mesh"]["path"] =  f"3Dsimulation_data/{energy_function}/{scenario}/unloaded_geo.hdf5"
+    simulation_params["mesh"]["microstructure"] =  f"3Dsimulation_data/{energy_function}/{scenario}/unloaded_geo_microstructure.h5"
+    simulation_params["mechdata"] = f"3Dsimulation_data/pressure_traces/{scenario}_traces.csv"
+    simulation_params["elasticity_estimation"]["lower_bound"] = list(experiment_params["optimization_lower_bounds"][scenario][energy_function].values()) #Update in the data
 
-        #Pick scenario appropriate boundary conditions and compressibility
-        simulation_params["numerics"]["base_spring_k"] =  simulation_params["numerics"]["base_spring_k"][scenario]
-        simulation_params["numerics"]["compress"] =  simulation_params["numerics"]["compress"][scenario]
+    for i, lhs_params in lhs_subsample.iterrows():
+        simulation_params["output"]["files"]["path"] = f"results/{energy_function}/{scenario}/{i}"
+        simulation_params["output"]["files"]["logfile"] = f"results/{energy_function}/{scenario}/{i}/parameter_estimation.log"
+        simulation_params["material_parameters"] = dict(lhs_params)
 
         simpleheartlogger.logpath = f"results/{energy_function}/{i}/paramest.log"
-
-        simulation_params["elasticity_estimation"]["lower_bound"] = list(experiment_params["optimization_lower_bounds"][scenario][energy_function].values()) #Update in the data
+        
         simulation_params["elasticity_estimation"]["displacement_db"] = f"3Dsimulation_data/{energy_function}/{scenario}/noisy_disps/{i}_{DISP_NOISE}.hdf5"
         
         simpleheartlogger.log(pretty_print_dict(simulation_params))
